9ab6139ac0ff7c54b0197ecc66ebb3cb1f921e31
Breno Santin <brenolimaa96@gmail.com>
2022-11-26 14:07:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import './index.css';
+import drink from "./assets/drinkpng.png";
+
+function App() {
+
+  return (
+    <div>
+      <div className="drinks-header">
+        <h2>Drinks mais badalados</h2>
+      </div>
+
+      <section className='cocktail-section'>
+        <div className='cocktail-container'>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          <div className='cocktail-item'>
+            <div className="cocktail-inner">
+              <div className="cocktail-image-div">
+              <img src={drink} alt="" />
+              </div>
+              <div className="cocktail-name">Drink Aeqoes</div>
+              <div className="cocktail-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde, adipisci a tenetur hic repellendus iure pariatur asperiores corrupti ipsa quidem recusandae perferendis aspernatur, assumenda illum! Eaque sed rerum eveniet.
+                Aspernatur beatae saepe sequi dolorem corporis ipsa, iste deserunt reiciendis quis nemo. Illo iste veritatis sint neque, exercitationem sit inventore ducimus, soluta, rerum dolores quasi doloremque eum commodi consequatur et.</div>
+            </div>
+          </div>
+          
+         
+        </div>
+      </section>
+
+
+
+    </div>
+  )
+}
+
+export default App
